b14109c76fc9099e7a7d82c165966fb6359850c0
recfordrem <rbarinov@diasoft.ru>
2024-10-15 14:21:33 +0300

    Added gateway functionality

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,42 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ru.practicum.shareit.user.dto.UserRequestDto;
+
+
+@Controller
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class UserController {
+    private final UserClient userClient;
+
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody UserRequestDto requestDto) {
+        return userClient.createUser(requestDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@RequestBody UserRequestDto requestDto,
+                                             @PathVariable Long userId) {
+        return userClient.updateUser(requestDto, userId);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUser(@PathVariable Long userId) {
+        return userClient.getUser(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long userId) {
+        return userClient.deleteUser(userId);
+    }
+}
